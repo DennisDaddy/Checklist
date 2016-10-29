ab14164708980ce818bc0ba753539d44eae2bbe8
DennisDaddy <softteez95@gmail.com>
2016-10-29 21:52:05 +0300

    add item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,12 @@ class ItemsController < ApplicationController
 	end
 
 	def create
-		
+		@item = Item.new(params.require(:item).permit(:title, :description))
+		if @item.save
+			redirect_to root_path
+		else
+			render 'new'
+			
+		end
 	end
 end
